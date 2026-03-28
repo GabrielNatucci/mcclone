bb470554823913e63bb3f41ed7340e35ebf3cc7e
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2026-03-28 09:02:35 -0300

    adicionando wireframe e f3

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,8 @@ pub fn main() !void {
     rl.initWindow(1920, 1080, "MINECLONE");
 
     var camera = std.mem.zeroes(rl.Camera);
-    camera.position = rl.Vector3{ .x = 0.0, .y = 10.0, .z = 10.0 };
-    camera.target = rl.Vector3{ .x = 0.0, .y = 0.0, .z = 0.0 };
+    camera.position = rl.Vector3{ .x = 0.0, .y = 100.0, .z = 10.0 };
+    // camera.target = rl.Vector3{ .x = 0.0, .y = 0.0, .z = 0.0 };
     camera.up = rl.Vector3{ .x = 0.0, .y = 1.0, .z = 0.0 };
     camera.fovy = 75.0;
     camera.projection = rl.CameraProjection.perspective;
@@ -24,16 +24,38 @@ pub fn main() !void {
 
     var world: World = try World.init(allocator);
 
+    var wireframe = false;
+
     while (!rl.windowShouldClose()) {
+        if (rl.isKeyPressed(rl.KeyboardKey.f10)) {
+            wireframe = !wireframe; // toggle
+        }
+
+        if (camera.position.y > 5) {
+            camera.position.y -= 1;
+        }
+
         rl.updateCamera(&camera, rl.CameraMode.first_person);
 
         rl.beginDrawing();
         rl.clearBackground(.white);
         rl.beginMode3D(camera);
 
+        if (wireframe) {
+            rl.gl.rlEnableWireMode();
+        }
+
         try world.render(atlas);
 
+        if (wireframe) {
+            rl.gl.rlDisableWireMode();
+        }
+
         rl.endMode3D();
+
+        var text_buf: [128]u8 = undefined;
+        const post_text = try std.fmt.bufPrintZ(&text_buf, "PLAYER POSITION\n X:{d:.2}\n Y:{d:.2}\n Z:{d:.2}\n", .{ camera.position.x, camera.position.y, camera.position.z });
+        rl.drawText(post_text, 10, 30, 20, .black);
 
         rl.drawFPS(10, 10);
         rl.endDrawing();
